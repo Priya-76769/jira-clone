0963327d49adc41320c339f60189a26a1f8b898d
Priya-76769 <167970130+Priya-76769@users.noreply.github.com>
2024-06-13 15:38:39 +0530

    changes to be commited

---
+++ var.js
@@ -0,0 +1,12 @@
+console.log("hello world!skill bridge");
+console.log("nexzera");
+let a=2;
+let b=3.14;
+let c=true;
+let d="var"
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+let e=a+b;
+console.log(e);
